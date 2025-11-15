caca0c7fe0bc43abd8ee941ee4dce9598f076eaa
EilaChris <eila_cristiane@yahoo.com>
2025-11-15 16:54:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ fig1=px.line(df, x='Date_reported', y='Cumulative_cases', color='Country', title
 fig1.update_layout(xaxis_title='Data',yaxis_title='Número de Casos Acumulados')
 fig1.show()
 
+df_brasil_india = df.query('Country == "India" or Country== "Brazil"')
+fig2=px.line(df_brasil_india,x='Date_reported', y='Cumulative_cases', color='Country', title='Números de casos de Covid no Brasil')
+fig2.update_layout(xaxis_title='Data',yaxis_title='Número de Casos Acumulados no Brasil e na Índia')
+fig2.show()
+
 st.set_page_config(page_title="DashCovid",layout="wide")
 
 st.plotly_chart(fig1, use_container_width=True)
+st.plotly_chart(fig2, use_container_width=True)
